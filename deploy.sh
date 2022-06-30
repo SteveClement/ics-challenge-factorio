485f91000c3f1727308e909d8d806d9acaca93b0
Steve Clement <steve@localhost.lu>
2022-06-30 18:13:22 +0200

    chg: [sh] Tuned deploy file to generate zip file

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-VERSION="0.0.1"
-rsync -v -r --delete --exclude .git\* --exclude README.md --exclude images --exclude deploy.sh ../ics-challenge-factorio_${VERSION} ~/.factorio/mods/
+# Factorio support zip packages as mods. This will be cleaner then rsyncing.
+
+
+VERSION=$(jq -r .version info.json)
+
+# Figure out changelog format
+##./venv/bin/gitchangelog > changelog.txt
+
+# rsync -v -r --delete --exclude venv --exclude .git\* --exclude README.md --exclude images --exclude deploy.sh ../ics-challenge-factorio_${VERSION} ~/.factorio/mods/
+cd ../
+zip -v -r /tmp/ics-challenge-factorio_${VERSION}.zip ics-challenge-factorio -x ics-challenge-factorio/venv/\* ics-challenge-factorio/.git\* ics-challenge-factorio/README.md ics-challenge-factorio/images\* ics-challenge-factorio/deploy.sh
+mv /tmp/ics-challenge-factorio_${VERSION}.zip ~/.factorio/mods/
