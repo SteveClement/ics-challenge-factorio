5d0da153bda7cf178c3b03a2de42af079d4cc29d
Steve Clement <steve@localhost.lu>
2022-06-30 19:23:27 +0200

    chg: [sh] Updated deploy script to be x-platform and more checks

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,31 @@
 
 # Factorio support zip packages as mods. This will be cleaner then rsyncing.
 
+# MacOS shit:
+
+#OIFS="$IFS"
+#IFS=$'\n'
+# Bash debug:
+# + mv /tmp/ics-challenge-factorio_0.0.1.zip '~/Library/Application' Support/factorio/mods/i
+# mv: Support/factorio/mods/ is not a directory
+# ~ not resolving - Use eval
+
+[[ ! -d ./venv/ ]] && echo "Make sure the Python Virtualenv exists"
+
+JQ=$(jq --version > /dev/null 2> /dev/null; echo $?)
+[[ ${JQ} != 0 ]] && echo "Please install jq"
 
 VERSION=$(jq -r .version info.json)
+MOD_BASE="ics-challenge-factorio"
+OS=$(uname -s)
+
+[[ ${OS} == "Darwin" ]] && MOD_DIR="~/Library/ApplicationSupport/factorio/mods/"
+[[ ${OS} == "Linux" ]] && MOD_DIR="~/.factorio/mods/"
 
 # Figure out changelog format
 ##./venv/bin/gitchangelog > changelog.txt
 
 # rsync -v -r --delete --exclude venv --exclude .git\* --exclude README.md --exclude images --exclude deploy.sh ../ics-challenge-factorio_${VERSION} ~/.factorio/mods/
 cd ../
-zip -v -r /tmp/ics-challenge-factorio_${VERSION}.zip ics-challenge-factorio -x ics-challenge-factorio/venv/\* ics-challenge-factorio/.git\* ics-challenge-factorio/README.md ics-challenge-factorio/images\* ics-challenge-factorio/deploy.sh
-mv /tmp/ics-challenge-factorio_${VERSION}.zip ~/.factorio/mods/
+zip -v -r /tmp/${MOD_BASE}_${VERSION}.zip ${MOD_BASE} -x ${MOD_BASE}/venv/\* ${MOD_BASE}/.git\* ${MOD_BASE}/README.md ${MOD_BASE}/images\* ${MOD_BASE}/deploy.sh
+eval mv /tmp/${MOD_BASE}_${VERSION}.zip ${MOD_DIR}
